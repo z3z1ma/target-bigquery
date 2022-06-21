2e04803ebb98ea0a994a7554dcc94e7b566d3372
z3z1ma <butler.alex2010@gmail.com>
2022-06-21 11:51:03 -0500

    add three different sink possibilities

--- target_bigquery/sinks.py
+++ target_bigquery/sinks.py
@@ -2,11 +2,13 @@
 import datetime
 import json
 import re
-from io import BytesIO
-from typing import Dict, List, Optional
+from concurrent.futures import ThreadPoolExecutor
+from io import BufferedWriter, BytesIO
+from typing import Any, Dict, List, Optional, cast
 
 import orjson
-from google.cloud import bigquery
+import smart_open
+from google.cloud import bigquery, storage
 from memoization import cached
 from singer_sdk import PluginBase
 from singer_sdk.sinks import BatchSink
@@ -46,13 +48,19 @@ def safe_column_name(name: str, quotes: bool = False) -> str:
 
 
 @cached
-def get_client(credentials_path: str) -> bigquery.Client:
+def get_bq_client(credentials_path: str) -> bigquery.Client:
     """Returns a BigQuery client. Singleton."""
     return bigquery.Client.from_service_account_json(credentials_path)
 
 
-class BigQuerySink(BatchSink):
-    """BigQuery target sink class."""
+@cached
+def get_gcs_client(credentials_path: str) -> storage.Client:
+    """Returns a BigQuery client. Singleton."""
+    return storage.Client.from_service_account_json(credentials_path)
+
+
+class BaseBigQuerySink(BatchSink):
+    """Base BigQuery target sink class."""
 
     def __init__(
         self,
@@ -62,7 +70,7 @@ class BigQuerySink(BatchSink):
         key_properties: Optional[List[str]],
     ) -> None:
         super().__init__(target, stream_name, schema, key_properties)
-        self._client = get_client(self.config["credentials_path"])
+        self._client = get_bq_client(self.config["credentials_path"])
         self._table = (
             f"{self.config['project']}.{self.config['dataset']}.{self.stream_name}"
         )
@@ -72,10 +80,13 @@ class BigQuerySink(BatchSink):
             self.jsonschema_prop_to_bq_column(name, prop)
             for name, prop in self.schema["properties"].items()
         ]
+        # Track Jobs
+        self.job_queue = []
+        self.executor = ThreadPoolExecutor(self.config["threads"])
 
     @property
     def max_size(self) -> int:
-        return self.config.get("max_size", 100)
+        return self.config.get("batch_size_limit", 15000)
 
     def jsonschema_prop_to_bq_column(
         self, name: str, schema_property: dict
@@ -145,7 +156,7 @@ class BigQuerySink(BatchSink):
             new_schema = original_schema[:]
             new_schema.extend(fields_to_add)
             table.schema = new_schema
-            table = self._client.update_table(table, ["schema"])
+            self._client.update_table(table, ["schema"])
 
     def _parse_json_with_props(self, record: dict, _prop_spec=None):
         """Recursively serialize a JSON object which has props that were forcibly coerced
@@ -188,6 +199,10 @@ class BigQuerySink(BatchSink):
             return self._parse_json_with_props(record)
         return record
 
+
+class BigQueryStreamingSink(BaseBigQuerySink):
+    """BigQuery Streaming target sink class."""
+
     def process_record(self, record: dict, context: dict) -> None:
         """Write record to buffer"""
         self.records_to_drain.append(record)
@@ -209,18 +224,16 @@ class BigQuerySink(BatchSink):
         json.dumps = cached_fn
 
         if errors == []:
-            self.logger.info("New rows have been added.")
+            self.logger.info("New rows have been added to %s.", self.stream_name)
         else:
-            self.logger.info(
-                "Encountered errors while inserting rows: {}".format(errors)
-            )
+            self.logger.info("Encountered errors while inserting rows: %s", errors)
 
         self.mark_drained()
         self.records_to_drain = []
 
 
-class BigQueryBufferSink(BigQuerySink):
-    """BigQuery target sink class."""
+class BigQueryBatchSink(BaseBigQuerySink):
+    """BigQuery Batch target sink class."""
 
     def process_record(self, record: dict, context: dict) -> None:
         """Write record to buffer"""
@@ -244,7 +257,7 @@ class BigQueryBufferSink(BigQuerySink):
         data: BytesIO = context["records"]
 
         # Load the data into BigQuery
-        job = self._client.load_table_from_file(
+        job: bigquery.LoadJob = self._client.load_table_from_file(
             data,
             self._table,
             rewind=True,
@@ -260,12 +273,88 @@ class BigQueryBufferSink(BigQuerySink):
             ),
         )
 
-        # Wait for the load job to complete.
-        self.logger.info(f"Job created for {self.stream_name}, awaiting results")
-        r = job.result()
-        self.logger.info(
-            f"{r.job_id}: Loaded {self.current_size} rows into {self.stream_name}"
+        job.add_done_callback(
+            lambda resp: self.logger.info(
+                f"Batch load job {resp.job_id} for {self.stream_name} completed."
+            )
         )
+        self.job_queue.append(self.executor.submit(job.result))
+
+        # Flush the buffer
+        data.seek(0)
+        data.flush()
+        self.mark_drained()
+
+
+class BigQueryGcsStagingSink(BaseBigQuerySink):
+    """BigQuery Batch target sink class."""
+
+    def __init__(
+        self,
+        target: PluginBase,
+        stream_name: str,
+        schema: Dict,
+        key_properties: Optional[List[str]],
+    ) -> None:
+        super().__init__(target, stream_name, schema, key_properties)
+        self._gcs_client = get_gcs_client(self.config["credentials_path"])
+        self._base_blob_path = "gs://{}/{}/{}/{}".format(
+            self.config["bucket"],
+            self.config.get("prefix_override", "target_bigquery"),
+            self.config["dataset"],
+            self.stream_name,
+        )
+
+    def process_record(self, record: dict, context: dict) -> None:
+        """Write record to buffer"""
+        if not context.get("records"):
+            context["records"] = BytesIO()
+        context["records"].write(
+            orjson.dumps(
+                record,
+                option=orjson.OPT_APPEND_NEWLINE,
+            )
+        )
+
+    def process_batch(self, context: dict) -> None:
+        """Write out any prepped records and return once fully written."""
+
+        if self.current_size == 0:
+            return
+        self.start_drain()
+
+        # Get the reference to the buffer
+        data: BytesIO = context["records"]
+        target_stage = f"{self._base_blob_path}/{context['batch_id']}.jsonl"
+
+        # Load the data into GCS Stage
+        with smart_open.open(
+            target_stage, "wb", transport_params={"client": self._gcs_client}
+        ) as fh:
+            data.seek(0)
+            cast(BufferedWriter, fh).write(data.getbuffer())
+
+        # Load the data into BigQuery
+        job: bigquery.LoadJob = self._client.load_table_from_uri(
+            target_stage,
+            self._table,
+            timeout=self.config["timeout"],
+            job_config=bigquery.LoadJobConfig(
+                source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+                schema_update_options=[
+                    # bigquery.SchemaUpdateOption.ALLOW_FIELD_RELAXATION,
+                    bigquery.SchemaUpdateOption.ALLOW_FIELD_ADDITION,
+                ],
+                ignore_unknown_values=True,
+            ),
+        )
+
+        job.add_done_callback(
+            lambda resp: self.logger.info(
+                f"GCS batch to BigQuery job {resp.job_id} for {self.stream_name} completed."
+            )
+        )
+        self.job_queue.append(self.executor.submit(job.result))
 
         # Flush the buffer
         data.seek(0)
--- target_bigquery/target.py
+++ target_bigquery/target.py
@@ -1,9 +1,14 @@
 """BigQuery target class."""
+from typing import Type
 
 from singer_sdk import typing as th
-from singer_sdk.target_base import Target
+from singer_sdk.target_base import Sink, Target
 
-from target_bigquery.sinks import BigQuerySink
+from target_bigquery.sinks import (
+    BigQueryBatchSink,
+    BigQueryGcsStagingSink,
+    BigQueryStreamingSink,
+)
 
 
 class TargetBigQuery(Target):
@@ -15,16 +20,19 @@ class TargetBigQuery(Target):
             "credentials_path",
             th.StringType,
             description="The path to a gcp credentials json file.",
+            required=True,
         ),
         th.Property(
             "project",
             th.StringType,
             description="The target GCP project to materialize data into.",
+            required=True,
         ),
         th.Property(
             "dataset",
             th.StringType,
             description="The target dataset to materialize data into.",
+            required=True,
         ),
         th.Property(
             "add_record_metadata",
@@ -32,5 +40,38 @@ class TargetBigQuery(Target):
             description="Inject record metadata into the schema.",
             default=True,
         ),
+        th.Property(
+            "batch_size_limit",
+            th.IntegerType,
+            description="The maximum number of rows to send in a single batch.",
+            default=10000,
+        ),
+        th.Property(
+            "threads",
+            th.IntegerType,
+            description="The number of threads to use for writing to BigQuery.",
+            default=8,
+        ),
+        th.Property(
+            "method",
+            th.StringType,
+            description="The method to use for writing to BigQuery. Accepted values are: batch, stream, gcs",
+            default="batch",
+        ),
+        th.Property(
+            "bucket",
+            th.StringType,
+            description="The GCS bucket to use for staging data. Only used if method is gcs.",
+        ),
     ).to_dict()
-    default_sink_class = BigQuerySink
+
+    def get_sink_class(self, stream_name: str) -> Type[Sink]:
+        method = self.config.get("method", "batch")
+        if method == "batch":
+            return BigQueryBatchSink
+        elif method == "stream":
+            return BigQueryStreamingSink
+        elif method == "gcs":
+            return BigQueryGcsStagingSink
+        else:
+            raise ValueError(f"Unknown method: {method}")
