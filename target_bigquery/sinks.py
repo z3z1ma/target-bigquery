f01e6cd62992e51314c0f9824ccb6bab92b00554
z3z1ma <butler.alex2010@gmail.com>
2022-07-14 11:14:26 -0700

    use enum for schema declarations

--- target_bigquery/sinks.py
+++ target_bigquery/sinks.py
@@ -56,32 +56,32 @@ class BaseBigQuerySink(BatchSink):
             ),
             bigquery.SchemaField(
                 "_sdc_extracted_at",
-                "TIMESTAMP",
+                bigquery.SqlTypeNames.TIMESTAMP,
                 description="Timestamp indicating when the record was extracted the record from the source",
             ),
             bigquery.SchemaField(
                 "_sdc_received_at",
-                "TIMESTAMP",
+                bigquery.SqlTypeNames.TIMESTAMP,
                 description="Timestamp indicating when the record was received by the target for loading",
             ),
             bigquery.SchemaField(
                 "_sdc_batched_at",
-                "TIMESTAMP",
+                bigquery.SqlTypeNames.TIMESTAMP,
                 description="Timestamp indicating when the record's batch was initiated",
             ),
             bigquery.SchemaField(
                 "_sdc_deleted_at",
-                "TIMESTAMP",
+                bigquery.SqlTypeNames.TIMESTAMP,
                 description="Passed from a Singer tap if DELETE events are able to be tracked. In general, this is populated when the tap is synced LOG_BASED replication. If not sent from the tap, this field will be null.",
             ),
             bigquery.SchemaField(
                 "_sdc_sequence",
-                "BIGINT",
+                bigquery.SqlTypeNames.INT64,
                 description="The epoch (milliseconds) that indicates the order in which the record was queued for loading",
             ),
             bigquery.SchemaField(
                 "_sdc_table_version",
-                "INT",
+                bigquery.SqlTypeNames.INTEGER,
                 description="Indicates the version of the table. This column is used to determine when to issue TRUNCATE commands during loading, where applicable",
             ),
         ]
